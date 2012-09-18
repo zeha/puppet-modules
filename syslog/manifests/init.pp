204eddeee5ede9b5bed0f968388467462e54b9a8
Christian Hofstaedtler <christian@hofstaedtler.name>
2012-09-18 13:58:11 +0200

    syslog: wheezy support

--- syslog/manifests/init.pp
+++ syslog/manifests/init.pp
@@ -2,18 +2,28 @@ class syslog {
   package { "rsyslog":
     ensure => installed,
   }
+  service { "rsyslog":
+    require => Package["rsyslog"]
+  }
   file { "/etc/rsyslog.conf":
     source => "puppet:///modules/syslog/rsyslog.conf",
     mode   => 0644,
     owner  => root,
     group  => root,
     ensure => present,
+    notify => Service["rsyslog"]
   }
   file { "/etc/logrotate.d/rsyslog":
-    source => "puppet:///modules/syslog/logrotate",
+    source => ["puppet:///modules/syslog/logrotate.${lsbdistcodename}", "puppet:///modules/syslog/logrotate"],
     mode   => 0644,
     owner  => root,
     group  => root,
-    ensure => present,
+    ensure => present
+  }
+  file { "/var/spool/rsyslog":
+    mode   => 0755,
+    owner  => root,
+    group  => root,
+    ensure => directory
   }
 }
