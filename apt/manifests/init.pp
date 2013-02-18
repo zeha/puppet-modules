5f3b623abc623faf96142d9b6c327890c57ef670
Christian Hofstaedtler <christian@hofstaedtler.name>
2013-02-18 13:26:32 +0100

    split unattendedupgrades out of apt/init.pp

--- apt/manifests/init.pp
+++ apt/manifests/init.pp
@@ -24,14 +24,3 @@ class apt {
   }
 
 }
-
-class apt::unattendedupgrades {
-  package { "unattended-upgrades":
-    ensure => installed
-  }
-  file { "/etc/apt/apt.conf.d/50unattended-upgrades":
-    ensure => present,
-    mode   => 0644,
-    source => "puppet:///modules/apt/50unattended-upgrades.${lsbdistcodename}"
-  }
-}
