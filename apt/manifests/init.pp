17beaf45948f533d8faf51967e9441f9b4811451
Christian Hofstaedtler <christian@hofstaedtler.name>
2013-02-18 13:27:55 +0100

    merge modernized APT module

--- apt/manifests/init.pp
+++ apt/manifests/init.pp
@@ -7,6 +7,7 @@ class apt {
   }
 
   exec { "apt-get update":
+    path   => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"],
     refreshonly => true
   }
 
@@ -22,5 +23,21 @@ class apt {
     source => "puppet:///modules/apt/sources.list.${lsbdistcodename}",
     notify => Exec["apt-get update"]
   }
+}
 
+define apt::key($source, $ensure, $keyid) {
+  file { "/etc/apt/key-${name}.asc":
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    ensure  => $ensure,
+    source  => $source,
+    notify  => Exec["apt-key add /etc/apt/key-${name}.asc"],
+  }
+  exec { "apt-key add /etc/apt/key-${name}.asc":
+    require => File["/etc/apt/key-${name}.asc"],
+    unless  => "apt-key adv --list-keys $keyid",
+    path    => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"],
+    notify  => Exec["apt-get update"],
+  }
 }
